f3af94d228e297028c70f20b28ad1529c2b514aa
Eric Lee <ayrocklee@gmail.com>
2023-07-17 20:20:17 -0700

    use `undefined` for empty cursor in prisma

--- services/workout.js
+++ services/workout.js
@@ -3,8 +3,8 @@ const prisma = require('../lib/prisma');
 async function getWorkoutsForUser(userId, { take, cursor } = {}) {
   return await prisma.workout.findMany({
     take,
-    skip: cursor ? 1 : null, // Skip the cursor
-    cursor: cursor ? { id: cursor } : null,
+    skip: cursor ? 1 : undefined, // Skip the cursor
+    cursor: cursor ? { id: cursor } : undefined,
     where: {
       userId,
     },
